8d236ca06dc151a4ddd92af4476fd1cc3a159442
JoacssGL <141186589+JoacssGL@users.noreply.github.com>
2023-09-11 00:42:39 -0300

    Completo el código

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const apiUrl = "https://www.themealdb.com/api/json/v1/1";
+
+fetch(`${apiUrl}/search.php?s=chicken`)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Error: Código de estado ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    const meals = data.meals;
+    if (meals) {
+      meals.forEach((meal) => {
+        console.log(`Nombre de la receta: ${meal.strMeal}`);
+      });
+    } else {
+      console.log("No se encontraron recetas.");
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
